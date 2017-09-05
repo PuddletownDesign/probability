41d98ff22aac4046792a572eebb16f8625fb7ea0
Karli <karrlii@gmail.com>
2017-09-05 14:54:33 -0700

    updated coin flipping function with user input and basically changed whole implementation

--- r-coin-flip/karli/coin-flip.r
+++ r-coin-flip/karli/coin-flip.r
@@ -1,17 +1,46 @@
+# This program calculates the probability of getting all heads and 
+# the probability of getting at least one tails when flipping a coin x number of times 
+
 coin.flip <- function(flips) {
-	p = 1 / ( flips * 2 ) * 100
-	print(p)
-	return(p)
+	flipOutcomes = 2
+	numberofWays = flipOutcomes ** flips
+
+	allHeads = 1 / numberofWays
+	atLeastOneTails = 1 - ( 1 / numberofWays)
+
+	allHeadsPercent = format(round(allHeads * 100, 2), nsmall = 2)
+	atLeastOneTailsPercent = format(round(atLeastOneTails * 100, 2), nsmall = 2)
+	
+	cat("Probability of getting all heads is: ", allHeadsPercent)
+	cat("\nProbability of getting at least one tails is: ", atLeastOneTailsPercent )
+	cat("\n\n\n")
+	
 }
 
-odds = c()
+message("Let's flip coins!\n\n")
 
-calculate.probabilty.of.sequence <- function(sequence="HHT", flips=10,  ...) {
-	for ( i in 1:flips ) {
-		odds <- append(coin.flip(flips), odds)
-	}
+message("Tell me how many times you want to flip a coin and I'll tell you what your chances are of getting ALL heads in a row!")
+message("PS. I'll also tell you what your chances are of getting a single tails :)")
+message("To stop flipping enter 0\n")
+
+# assign one to flips so we can enjoy the while loop
+flips <- 1
+
+while ( flips > 0 ) {
+	# get input from user
+	cat("How many times would you like to flip a coin: ")
+	flips <- readLines("stdin", 1) 
+
+	#convert character into integer
+	flips <- as.integer(flips)
+
+	# make sure turkey did not mess up entering a number
+	if ( is.na(flips) ) {
+		cat("Enter a number ffs:")
+		flips <- readLines("stdin", 1)
+	}	 
+	
+	coin.flip(flips)
 }
 
-odds <- calculate.probabilty.of.sequence("HH")
-print(odds)
-
+print("Good Bye!")
