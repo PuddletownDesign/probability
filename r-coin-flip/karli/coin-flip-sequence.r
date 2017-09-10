a5e0c38267592415514dc05e538e91d223b015aa
Karli <karrlii@gmail.com>
2017-09-10 05:24:43 -0700

    documented for clarity

--- r-coin-flip/karli/coin-flip-sequence.r
+++ r-coin-flip/karli/coin-flip-sequence.r
@@ -1,7 +1,11 @@
 # This program calculates the probability of getting exactly X number of heads
 # when flipping a coin Y number of times 
  
-
+# the factorial function takes a positive integer n and returns the product 
+# of the integers from 1 to n denoted by n! and read "n factorial"
+# n! = 1 * 2 * 3 ... n
+# R has a built in function factorial(x) which does the same thing
+# but because this is a n00b tutorial I decided for clarity to write my own
 calculate.factorial <-function (positiveInteger) {
 
 	counter = positiveInteger
@@ -16,6 +20,8 @@ calculate.factorial <-function (positiveInteger) {
 }
 
 
+# this function returns # of outcomes that have x number of heads
+# r has a function choose(n,k)  which does the same thing 
 calculate.heads.scenarios <- function(flips, heads) {
 	numberofHeadsScenarios = 0
 
@@ -30,9 +36,11 @@ calculate.heads.scenarios <- function(flips, heads) {
 	return(numberofHeadsScenarios)
 }
 
+# this function returns the probability of getting
+# x number of heads in x number of flips
 calculate.probability.of.sequence <- function(flips, heads) {
 
-	flipOutcomes = 2
+	flipOutcomes = 2 
 	numberofWaysFlips = flipOutcomes ** flips
 
 	numberofWaysHeads = calculate.heads.scenarios(flips, heads)
